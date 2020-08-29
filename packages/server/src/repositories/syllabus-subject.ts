e24071f048e01e3e97f9174a15939742fae7615a
f0reachARR <f0reach@f0reach.me>
2020-08-29 18:19:43 +0900

    Add subjects repository abstract

---
+++ packages/server/src/repositories/syllabus-subject.ts
@@ -0,0 +1,32 @@
+import { SyllabusSubjectEntity } from '../entities/syllabus-subject';
+
+export interface SyllabusSubjectRepositoryQuery {
+  date?: number;
+  hour?: number;
+  type?: 'intensive';
+  year?: number;
+  semester?: string;
+  available?: boolean;
+  flags?: Array<
+    'internship' | 'igp' | 'al' | 'pbl' | 'pt' | 'univ3' | 'kyoto' | 'lottery'
+  >;
+  title?: string;
+}
+export interface SyllabusSubjectRepositoryFindRequest {
+  query: SyllabusSubjectRepositoryQuery;
+  from: number;
+  count: number;
+}
+
+export interface SyllabusSubjectRepositoryFindResponse {
+  totalCount: number;
+  items: SyllabusSubjectEntity[];
+}
+
+export interface SyllabusSubjectRepository {
+  init(): Promise<void> | void;
+  get(id: number): Promise<SyllabusSubjectEntity | null>;
+  find(
+    request: SyllabusSubjectRepositoryFindRequest,
+  ): Promise<SyllabusSubjectRepositoryFindResponse>;
+}
