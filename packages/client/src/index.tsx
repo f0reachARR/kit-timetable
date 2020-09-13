13517e0c26216d0a5acf92a59752f73eba2f78d6
f0reachARR <f0reach@f0reach.me>
2020-09-13 21:39:42 +0900

    Add apollo client and provider

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,39 @@
+import {
+  InMemoryCache,
+  createHttpLink,
+  ApolloClient,
+  ApolloProvider,
+} from '@apollo/client';
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { RootView } from './views/Root';
+
+const cache = new InMemoryCache();
+const link = createHttpLink({
+  uri: location.origin + '/api/graphql',
+});
+
+const client = new ApolloClient({
+  cache: cache,
+  link: link,
+
+  queryDeduplication: false,
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all',
+    },
+  },
+});
 
 const main = document.querySelector('main');
 
-render(<RootView />, main);
+render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <RootView />
+    </BrowserRouter>
+  </ApolloProvider>,
+  main,
+);
--- packages/client/src/views/Root/index.tsx
+++ packages/client/src/views/Root/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Switch as RouterSwitch, BrowserRouter, Route } from 'react-router-dom';
+import { Switch as RouterSwitch, Route } from 'react-router-dom';
 import { Header } from '../../components/shared/Header';
 import { SubjectSearch } from '../SubjectSearch';
 
 export const RootView = () => {
   return (
-    <BrowserRouter>
-      <>
-        <Header />
-        <RouterSwitch>
-          <Route path='/search' component={SubjectSearch} />
-        </RouterSwitch>
-      </>
-    </BrowserRouter>
+    <>
+      <Header />
+      <RouterSwitch>
+        <Route path='/search' component={SubjectSearch} />
+      </RouterSwitch>
+    </>
   );
 };
