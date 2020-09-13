2d02c0b8e0b6f224e2fa948ff807bde30b59ae04
f0reachARR <f0reach@f0reach.me>
2020-09-14 00:38:02 +0900

    Add initial search query

--- packages/client/src/views/SubjectSearch/index.tsx
+++ packages/client/src/views/SubjectSearch/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import {
+  useFindSubjectLazyQuery,
+  SubjectSearchQuery,
+} from '../../api/graphql.generated';
 import { SearchSimpleForm } from '../../components/subject/SearchSimpleForm';
 
 export const SubjectSearch = () => {
+  const [query, setQuery] = React.useState<SubjectSearchQuery>({});
+  const [dispatchSearch, { called, loading }] = useFindSubjectLazyQuery();
+
+  const [debouncedSearch] = useDebouncedCallback(
+    (query: SubjectSearchQuery) => {
+      dispatchSearch({
+        variables: {
+          query,
+          from: 0,
+          count: 20,
+        },
+      });
+    },
+    500,
+  );
+
+  const handleQueryChange = React.useCallback(
+    (newQuery: SubjectSearchQuery) => {
+      setQuery(newQuery);
+      debouncedSearch(newQuery);
+    },
+    [setQuery],
+  );
+
   return (
     <div className='container max-w-screen-md mx-auto my-3 px-3'>
-      <SearchSimpleForm query={{}} isLoading onQueryChange={() => {}} />
+      <SearchSimpleForm
+        query={query}
+        isLoading={loading}
+        onQueryChange={handleQueryChange}
+      />
+      {called && <div>nyaa</div>}
     </div>
   );
 };
