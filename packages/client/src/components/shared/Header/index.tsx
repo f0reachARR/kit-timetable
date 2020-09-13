c2f0a814d86231ade3efe1b8b6ba421e2a823618
f0reachARR <f0reach@f0reach.me>
2020-09-13 14:23:57 +0900

    Add blueprintjs ui

---
+++ packages/client/src/components/shared/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Navbar, NavbarGroup } from '@blueprintjs/core';
+import React from 'react';
+export const Header = () => {
+  return (
+    <Navbar fixedToTop className='bp3-dark'>
+      <NavbarGroup align='left'></NavbarGroup>
+    </Navbar>
+  );
+};
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Header } from './components/shared/Header';
 
 const main = document.querySelector('main');
 
-render(<div />, main);
+render(
+  <>
+    <Header />
+  </>,
+  main,
+);
