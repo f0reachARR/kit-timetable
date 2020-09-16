1c0432d51d75cf00039521392a970fbbc672f29d
f0reachARR <f0reach@f0reach.me>
2020-09-16 23:21:54 +0900

    Improve list layout

--- packages/client/src/components/subject/SubjectListItem.tsx
+++ packages/client/src/components/subject/SubjectListItem.tsx
@@ -30,9 +30,11 @@ const createDateLabel = (item: SubjectSimpleFragment) => {
 
 export const SubjectListItem = ({ item }: { item: SubjectSimpleFragment }) => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between '>
       <div>
-        <h2 className='inline text-xl text-semibold truncate'>{item.title}</h2>
+        <h2 className='inline text-xl text-semibold truncate max-w-full'>
+          {item.title}
+        </h2>
         <h3 className='inline mx-3'>{item.class}</h3>
       </div>
       {item.credits && (
