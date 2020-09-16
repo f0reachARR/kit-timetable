e601f9f4d454079a3b7329c28685d95e5b9aee8b
f0reachARR <f0reach@f0reach.me>
2020-09-16 21:51:05 +0900

    Add extended info to subject list

--- packages/client/src/components/subject/SubjectListItem.tsx
+++ packages/client/src/components/subject/SubjectListItem.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
-import { SubjectSimpleFragment } from '../../api/graphql.generated';
+import {
+  SubjectSimpleFragment,
+  SubjectScheduleType,
+} from '../../api/graphql.generated';
+import { DATE_LABELS } from '../../constants';
+
+const createDateLabel = (item: SubjectSimpleFragment) => {
+  const category = item.categories[0];
+  if (
+    category.schedule.type === SubjectScheduleType.Fixed &&
+    category.schedule.days
+  ) {
+    if (category.schedule.days.length === 1) {
+      const { date, hour } = category.schedule.days[0];
+      return `${category.year}年 ${category.semester} ${DATE_LABELS[date]}曜 ${hour}限`;
+    } else {
+      const { date, hour } = category.schedule.days[0];
+      const { hour: lastHour } = category.schedule.days[
+        category.schedule.days.length - 1
+      ];
+      return `${category.year}年 ${category.semester} ${DATE_LABELS[date]}曜 ${hour}-${lastHour}限`;
+    }
+  }
+  if (category.schedule.type === SubjectScheduleType.Intensive) {
+    return `${category.year}年 ${category.semester} 集中`;
+  }
+  return '不明';
+};
 
 export const SubjectListItem = ({ item }: { item: SubjectSimpleFragment }) => {
   return (
@@ -7,7 +34,9 @@ export const SubjectListItem = ({ item }: { item: SubjectSimpleFragment }) => {
       <h2 className='inline text-xl text-semibold'>{item.title}</h2>
       <h3 className='inline mx-3'>{item.class}</h3>
       {item.credits && (
-        <span className='float-right leading-6'>{item.credits}単位</span>
+        <span className='float-right leading-6 invisible sm:visible'>
+          {createDateLabel(item)} {item.credits}単位
+        </span>
       )}
     </>
   );
