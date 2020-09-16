b16a4bb649dc965abe657a1d41f659cffd097a62
f0reachARR <f0reach@f0reach.me>
2020-09-16 19:29:07 +0900

    Refactor search page

--- packages/client/src/views/SubjectSearch/index.tsx
+++ packages/client/src/views/SubjectSearch/index.tsx
@@ -1,4 +1,4 @@
-import { NonIdealState } from '@blueprintjs/core';
+import { NonIdealState, Spinner } from '@blueprintjs/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDebounce } from 'use-debounce';
@@ -13,7 +13,9 @@ import { SubjectListItem } from '../../components/subject/SubjectListItem';
 export const SubjectSearch = () => {
   const [query, setQuery] = React.useState<SubjectSearchQuery>({});
   const [debouncedQuery] = useDebounce(query, 500);
-  const { refetch, loading, data } = useFindSubjectQuery();
+  const { refetch, loading, data, error } = useFindSubjectQuery({
+    notifyOnNetworkStatusChange: true,
+  });
 
   React.useEffect(() => {
     refetch({
@@ -33,6 +35,39 @@ export const SubjectSearch = () => {
     [setQuery],
   );
 
+  const renderSearchResult = React.useCallback(() => {
+    if (loading) {
+      return (
+        <div className='flex justify-center'>
+          <Spinner size={Spinner.SIZE_LARGE} />
+        </div>
+      );
+    }
+
+    if (error) {
+      return <NonIdealState icon='warning-sign' title='Error occured' />;
+    }
+
+    if (data?.subjects.total === 0) {
+      return <NonIdealState icon='search' title='No results' />;
+    }
+
+    return (
+      <>
+        <div>{data?.subjects.total}件</div>
+        <ul className='list-none'>
+          {data?.subjects.items.map((item) => (
+            <li key={item.id} className='border-gray-500 border-b py-2 md:px-1'>
+              <Link to={`/subject/${item.id}`} className='text-blue-500'>
+                <SubjectListItem item={item} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }, [loading, data, error]);
+
   return (
     <div className='container max-w-screen-md mx-auto my-3 px-3'>
       <SearchSimpleForm
@@ -41,26 +76,7 @@ export const SubjectSearch = () => {
         onQueryChange={handleQueryChange}
       />
       <SearchDetailsForm query={query} onQueryChange={handleQueryChange} />
-      {!loading &&
-        (data?.subjects.total === 0 ? (
-          <NonIdealState icon='search' title='No results' />
-        ) : (
-          <>
-            <div>{data?.subjects.total}件</div>
-            <ul className='list-none'>
-              {data?.subjects.items.map((item) => (
-                <li
-                  key={item.id}
-                  className='border-gray-500 border-b py-2 md:px-1'
-                >
-                  <Link to={`/subject/${item.id}`} className='text-blue-500'>
-                    <SubjectListItem item={item} />
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </>
-        ))}
+      {renderSearchResult()}
     </div>
   );
 };
