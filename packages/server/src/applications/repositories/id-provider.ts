cbfcbbd33bc4f67e373c641287898e5cb495bee8
f0reachARR <f0reach@f0reach.me>
2020-09-26 13:05:16 +0900

    Update id provider repository
    resolve internal id in id provider

--- packages/server/src/applications/repositories/id-provider.ts
+++ packages/server/src/applications/repositories/id-provider.ts
@@ -4,6 +4,13 @@ export interface IdProviderStartData {
 }
 
 export interface IdProviderRepository {
+  type: string;
   start(): Promise<IdProviderStartData>;
   getUserId(verifier: string, state: string): Promise<string>;
+
+  getInternalUserId(userId: string): Promise<string | null>;
+  associateInternalUserId(
+    userId: string,
+    internalUserId: string,
+  ): Promise<void>;
 }
